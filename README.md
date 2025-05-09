317224c3d1c609fe835a8207372e5000cd2d4d83
Harsh <harshmann8008@gmail.com>
2025-05-09 14:20:22 +0530

    Update README and exploring routes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DevTinder-backend
+
+working on DevTinder platfrom in which devs can interact with each other
--- src/app.js
+++ src/app.js
@@ -1,15 +1,33 @@
 const express = require('express');
-
 const app = express();
+const port = 7777;
 
-app.use("/help", (req, res) => {
+app.get("/user",(req,res) => {
+    res.send({firstname: 'harsh' , lastname : 'mann'})
+})
+
+app.post("/user", (req,res) => {
+    //saving data to db
+    res.send("data is successfully saved to DB")
+})
+
+app.patch("/user", (req,res) =>{
+    res.send("data is updated to DB")
+})
+
+app.delete("/user", (req,res) =>{
+    res.send("data is deleted from DB")
+})
+
+//this will match all the http method API call to /test
+app.use("/test", (req, res) => {
     res.send("hello from server!")
 })
 
-app.use("/", (req, res) => {
-    res.send("hello from harsh!")
-})
+// app.use("/", (req, res) => {
+//     res.send("hello from harsh!")
+// })
     
-app.listen(7777 , () =>{
-    console.log("project starting on port 7777....");
+app.listen(port , () =>{
+    console.log(`project starting on port ${port}....`);
 });
