7f7b45fd22e87ae664a7ad78959a5c93d31e10b7
Harsh <harshmann8008@gmail.com>
2025-05-10 18:20:57 +0530

    downgrade the express version and learning advance routing

--- src/app.js
+++ src/app.js
@@ -2,21 +2,16 @@ const express = require('express');
 const app = express();
 const port = 7777;
 
-app.get("/user",(req,res) => {
-    res.send({firstname: 'harsh' , lastname : 'mann'})
+app.get(/.*fly$/, (req, res) => {
+    res.send({ firstname: 'harsh', lastname: 'mann' })
 })
 
-app.post("/user", (req,res) => {
-    //saving data to db
-    res.send("data is successfully saved to DB")
-})
-
-app.patch("/user", (req,res) =>{
-    res.send("data is updated to DB")
-})
-
-app.delete("/user", (req,res) =>{
-    res.send("data is deleted from DB")
+app.get("/user?/:userid/blo+g/:blogid", (req, res) => {
+    console.log(req.params)
+    const {userid, blogid} = req.params;
+    console.log(req.query)
+    const name = req.query.name;
+    res.send({ userid: `${userid}`, blogid: `${blogid}`, name: `${name}`})
 })
 
 //this will match all the http method API call to /test
@@ -24,10 +19,6 @@ app.use("/test", (req, res) => {
     res.send("hello from server!")
 })
 
-// app.use("/", (req, res) => {
-//     res.send("hello from harsh!")
-// })
-    
-app.listen(port , () =>{
+app.listen(port, () => {
     console.log(`project starting on port ${port}....`);
 });
