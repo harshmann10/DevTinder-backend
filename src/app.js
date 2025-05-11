f969e2d767e6e3288dfb48b65a5e72d1b42f83fc
Harsh <harshmann8008@gmail.com>
2025-05-11 23:59:58 +0530

    Multiple Route Handlers and next parameter

--- src/app.js
+++ src/app.js
@@ -1,23 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 7777;
 
 app.get(/.*fly$/, (req, res) => {
-    res.send({ firstname: 'harsh', lastname: 'mann' })
-})
+    res.send({ firstname: "harsh", lastname: "mann" });
+});
 
 app.get("/user?/:userid/blo+g/:blogid", (req, res) => {
-    console.log(req.params)
+    console.log(req.params);
     const {userid, blogid} = req.params;
-    console.log(req.query)
+    console.log(req.query);
     const name = req.query.name;
-    res.send({ userid: `${userid}`, blogid: `${blogid}`, name: `${name}`})
-})
+    res.send({ userid: `${userid}`, blogid: `${blogid}`, name: `${name}` });
+});
 
-//this will match all the http method API call to /test
-app.use("/test", (req, res) => {
-    res.send("hello from server!")
-})
+app.use("/user",
+    [(req, res, next) => {
+        console.log("1st response");
+        next();
+        // res.send("hello from server!");
+    },
+    (req, res, next) => {
+        console.log("2st response");
+        // res.send("hello from server 2!");
+        next();
+    }],
+    (req, res, next) => {
+        console.log("3st response");
+        // res.send("hello from server 3!");
+        next();
+    },
+    (req, res, next) => {
+        console.log("4th response");
+        res.send("hello from server 4!");
+    }
+);
 
 app.listen(port, () => {
     console.log(`project starting on port ${port}....`);
