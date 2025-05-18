75390f4139f9ad9b8fc61464b36f656994260fdd
Harsh <harshmann8008@gmail.com>
2025-05-18 16:42:29 +0530

    get all user, user by email or id, delete user and update user by id or email

--- src/app.js
+++ src/app.js
@@ -6,10 +6,9 @@ const port = 7777;
 
 app.use(express.json()); // express.json middleware
 
-app.post("/signup", async (req, res) =>{
+app.post("/signup", async (req, res) => {
     //creating a new instance of the User model
     const user = new User(req.body);
-
     try {
         await user.save();
         res.send("User added succesfully");
@@ -17,6 +16,87 @@ app.post("/signup", async (req, res) =>{
         res.status(400).send("Error saving the User: " + err)
     }
 })
+
+//get user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        const users = await User.findOne({ emailId: userEmail })
+        // const users = await User.find({ emailId: userEmail })
+        if (users.length === 0) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(users)
+        }
+    } catch (err) {
+        res.status(400).send("something went wrong error - " + err);
+    }
+})
+
+//get user by id
+app.get("/userid", async (req, res) => {
+    const id = req.body.id;
+    try {
+        const users = await User.findById(id);
+        if(users.length === 0) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(users)
+        }
+    } catch (err) {
+        res.status(400).send("something went wrong " + err)
+    }
+})
+
+//Feed API - GET /feed- get all the users from the database
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users)
+    } catch (err) {
+        res.status(400).send("something went wrong error - " + err);
+    }
+})
+
+//delete the user by id
+app.delete("/userid", async (req, res) => {
+    const id = req.body.id;
+    try {
+        // await User.findOneAndDelete({_id : id})
+        await User.findByIdAndDelete(id)
+        res.send("user deleted succesfully")
+    } catch (err) {
+        res.status(400).send("something went wrong error - " + err);
+    }
+})
+
+//update the user data by id
+app.patch("/userid", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        // await User.findByIdAndUpdate(id, data);
+        const user = await User.findOneAndUpdate({_id : userId}, data, {returnDocument: "before"});
+        console.log(user);
+        res.send("user updated succesfully")
+    } catch (err) {
+        res.status(400).send("something went wrong error - " + err);
+    }
+})
+
+//update the user data by emailId
+app.patch("/user", async (req, res) => {
+    const email = req.body.email;
+    const data = req.body;
+    try {
+        const user = await User.findOneAndUpdate({ emailId: email }, data, { returnDocument: "before" });
+        console.log(user);
+        res.send("user updated succesfully")
+    } catch (err) {
+        res.status(400).send("something went wrong error - " + err);
+    }
+})
+
 
 connectDB().then(() => {
     console.log("Database connection established");
