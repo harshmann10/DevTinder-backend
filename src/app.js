129220cdd0222984b607955c00f5216c1590391a
Harsh <harshmann8008@gmail.com>
2025-07-02 19:52:24 +0530

    update CORS configuration to use it

--- src/app.js
+++ src/app.js
@@ -6,10 +6,11 @@ const cors = require("cors");
 require("dotenv").config();
 
 const port = process.env.PORT;
+const allowedOrigin = process.env.ALLOWED_ORIGIN || "http://localhost:5173";
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: allowedOrigin,
         credentials: true,
     })
 ); // CORS middleware
